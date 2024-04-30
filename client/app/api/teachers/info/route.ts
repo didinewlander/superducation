4365a2cb2afcaebb8d3384427906fb3c759c889b
Yedidya Newlander <ydidya.n@gmail.com>
2024-05-01 00:31:38 +0300

    missing api content

--- client/app/api/teachers/info/route.ts
+++ client/app/api/teachers/info/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GET(req:NextApiRequest, res:NextApiResponse){
-
+return res.json("This API is not working")
 }
